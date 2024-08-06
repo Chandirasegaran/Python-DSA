78f7474e081d662b8569dd0ce586951c052d5654
Chandirasegaran <chandirasegaransegar@gmail.com>
2024-08-06 23:28:11 +0530

    ++

--- Learn DSA/Trees/Binary Search Tree.py
+++ Learn DSA/Trees/Binary Search Tree.py
@@ -39,6 +39,56 @@ class BinarySearchTree:
                 return True
         return False
 
+    def BFS(self):
+        current_node = self.root
+        queue=[]
+        results=[]
+        queue.append(current_node)
+
+        while len(queue)>0:
+            current_node=queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+    
+    def dfs_pre_order(self):
+        results=[]
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results   
+         
+    def dfs_post_order(self):
+        results=[]
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results        
+    
+    def dfs_in_order(self):
+        results=[]
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results        
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
@@ -50,10 +100,15 @@ my_tree.insert(52)
 my_tree.insert(82)
 
 
-print(my_tree.root.left.value)
+# print(my_tree.root.left.value)
 
-print('BST Contains 21:')
-print(my_tree.contains(21))
+# print('BST Contains 21:')
+# print(my_tree.contains(21))
 
-print('\nBST Contains 17:')
-print(my_tree.contains(17))
+# print('\nBST Contains 17:')
+# print(my_tree.contains(17))
+
+print(my_tree.BFS())
+print(my_tree.dfs_pre_order())
+print(my_tree.dfs_post_order())
+print(my_tree.dfs_in_order())
